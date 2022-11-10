c8be5dee834737e59d8d14477ef5f880d0e92d33
Oladoke Ajibola Stephen <stephenoladoke6@gmail.com>
2022-11-10 04:05:53 -0800

    website maintennace

--- mail/contact.js
+++ mail/contact.js
@@ -73,26 +73,40 @@
 
 
 
+var currentDateTime = new Date();
+var year = currentDateTime.getFullYear();
+var month = (currentDateTime.getMonth() + 1);
+var date = (currentDateTime.getDate() + 1);
+
+if(date < 10) {
+  date = '0' + date;
+}
+if(month < 10) {
+  month = '0' + month;
+}
+
+var dateTomorrow = year + "-" + month + "-" + date;
+var checkinElem = document.querySelector("#checkin-date");
+var checkoutElem = document.querySelector("#checkout-date");
+
+checkinElem.setAttribute("min", dateTomorrow);
+
+checkinElem.onchange = function () {
+    checkoutElem.setAttribute("min", this.value);
+}
 
 
-// var currentDateTime = new Date();
-// var year = currentDateTime.getFullYear();
-// var month = (currentDateTime.getMonth() + 1);
-// var date = (currentDateTime.getDate() + 1);
-
-// if(date < 10) {
-//   date = '0' + date;
+// var todayDate = new Date();
+// var month = todayDate.getMonth();
+// var year = todayDate.getUTCFullYear() - 0;
+// var tdate = todayDate.getDate();
+// if (month < 10) {
+//     month = "0" + month
 // }
-// if(month < 10) {
-//   month = '0' + month;
+// if (tdate < 10) {
+//     tdate = "0" + tdate;
 // }
+// var maxDate = year + "-" + month + "-" + tdate;
+// document.getElementById("checkin-date").setAttribute("min", maxDate);
+// console.log(maxDate);
 
-// var dateTomorrow = year + "-" + month + "-" + date;
-// var checkinElem = document.querySelector("#checkin-date");
-// var checkoutElem = document.querySelector("#checkout-date");
-
-// checkinElem.setAttribute("min", dateTomorrow);
-
-// checkinElem.onchange = function () {
-//     checkoutElem.setAttribute("min", this.value);
-// }
